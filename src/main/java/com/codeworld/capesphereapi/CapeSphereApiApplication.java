b81f39bc3b8423f7cb5b53c9e3eea7e3abafc884
unknown <Capaciti@COMP3.bwb.org.za>
2023-11-30 13:50:04 +0200

    Files

---
+++ src/main/java/com/codeworld/capesphereapi/CapeSphereApiApplication.java
@@ -0,0 +1,13 @@
+package com.codeworld.capesphereapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapeSphereApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapeSphereApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeworld/capesphereapi/CapeSphereApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeworld.capesphereapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapeSphereApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
